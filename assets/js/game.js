0fe487bbc465c1c1d1e16f881d206b8264372287
GianniMejia <Giannimejia12@yahoo.com>
2022-04-04 04:51:47 -0700

    added var and console.log to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+function fight() {
+    var playerName = window.prompt("What is your robot's name?");
+    window.alert("The fight has begun!");
+    console.log("This logs a sting, good for leaving yourself a message")
+    console.log(10+10)
+    console.log("Our robot's name is " + playerName)
+
+}
+fight()
